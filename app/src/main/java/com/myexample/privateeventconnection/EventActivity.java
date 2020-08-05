23e8508ca377ae8aa8658becde66c016efb7b29b
Hongyi <hongyi@163.com>
2020-08-04 18:22:55 -0700

    add event dismiss function

--- app/src/main/java/com/myexample/privateeventconnection/EventActivity.java
+++ app/src/main/java/com/myexample/privateeventconnection/EventActivity.java
@@ -1,9 +1,11 @@
 package com.myexample.privateeventconnection;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,6 +39,7 @@ public class EventActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
     String uid;
+    boolean admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +53,7 @@ public class EventActivity extends AppCompatActivity {
         join = findViewById(R.id.eventjoin);
         edit = findViewById(R.id.edit);
         context = this;
-        //TODO ACCEPT EVENTNAME AND GROUPNAME
+
         Intent intent = getIntent();
         final String groupName = intent.getStringExtra("groupname");
         final String token = intent.getStringExtra("token");
@@ -68,11 +71,11 @@ public class EventActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //TODO 判断admin token 和 uid 是否一致 -> edit button 显示
+
         mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                boolean admin = false;
+                admin = false;
                 if(snapshot.child("Users").child(uid).child("Admin").getValue().toString().equals("1")){
                     admin = true;
                 }
@@ -89,45 +92,159 @@ public class EventActivity extends AppCompatActivity {
 
             }
         });
-        //TODO 判断这个event 是否还存在 如果不存在返回上一级
+
+
+        // If non-admin users are still in this activity but this event has been dismissed,
+        // send users to afterlogin activity
+
+        if(!admin){
+            FirebaseDatabase.getInstance()
+                    .getReference("Groups").child(groupName).child("Events").
+                    child(token).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.getValue() == null ){
+                        //go to AfterLoginActivity
+                        Intent intent = new Intent(EventActivity.this, AfterLoginActivity.class);
+                        //set this activity to the top of stack, so users cannot go back to dismissed events
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+
+
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+
+
+
 
         join.setText(btntext);
         final DatabaseReference reference = FirebaseDatabase.getInstance()
                 .getReference("Users").child(uid).child("Groups")
                 .child(groupName);
+
+        // If current user is admin, we need to dismiss the event and
+        // delete this event's information from database.
         join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(join.getText().toString().equals("Leave")){
-                    reference.child(token).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    FirebaseDatabase.getInstance().getReference().child("Groups").child(groupName).child("Events").child(token).addValueEventListener(new ValueEventListener() {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            join.setText("Join");
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Event event = dataSnapshot.child("EventInfo").getValue(Event.class);
+                            if(event != null){
+                                //current user is admin
+                                if(uid.equals(event.getAdmin())){
+                                    //prompt the dialog
+
+                                    AlertDialog.Builder altdial = new AlertDialog.Builder(EventActivity.this);
+                                    altdial.setMessage("You are the admin of this event. Do you want to dismiss?").setCancelable(false)
+                                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    //删除当前user下的event信息
+                                                    //reference.child(token).removeValue();
+
+                                                    //delete this event information from all users
+                                                    deleteAllUsersEvents(groupName, token);
+
+                                                    //delete this event information from all groups
+                                                    FirebaseDatabase.getInstance().getReference().child("Groups").child(groupName).child("Events").child(token).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                                            //go to AfterLoginActivity
+                                                            Intent intent = new Intent(EventActivity.this, AfterLoginActivity.class);
+                                                            //set this activity to the top of stack, so users cannot go back to dismissed events
+                                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                                            startActivity(intent);
+                                                        }
+                                                    });
+
+
+
+
+                                                }
+                                            })
+                                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    // do nothing
+                                                    dialog.cancel();
+                                                }
+                                            });
+
+                                    AlertDialog alertDialog = altdial.create();
+                                    alertDialog.setTitle("Default title");
+                                    alertDialog.show();
+
+
+
+                                }
+                                // current user is not admin
+                                else {
+                                    reference.child(token).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            join.setText("Join");
+                                        }
+                                    });
+
+                                }
+                            }
+
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
                         }
                     });
+
+
+
+
 
                 }else{
                     mDatabase = FirebaseDatabase.getInstance().getReference()
                             .child("Groups").child(groupName).child("Events").child(token);
+
+
+
                     mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            Event event = snapshot.child("EventInfo").getValue(Event.class);
-                            HashMap<String, String > hashMap = new HashMap<>();
-                            hashMap.put("Description", event.getDescription());
-                            hashMap.put("Admin", event.getAdmin());
-                            hashMap.put("EventName", event.getEventName());
-                            hashMap.put("EventTime", event.getEventName());
-                            hashMap.put("EventToken", event.getEventToken());
-                            hashMap.put("Location", event.getLocation());
-                            hashMap.put("Latitude", event.getLatitude());
-                            hashMap.put("Longitude", event.getLongitude());
-                            reference.child(token).setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    join.setText("Leave");
-                                }
-                            });
+                            //check null
+                            if(snapshot.child("EventInfo").getValue() != null){
+                                Event event = snapshot.child("EventInfo").getValue(Event.class);
+                                HashMap<String, String > hashMap = new HashMap<>();
+                                hashMap.put("Description", event.getDescription());
+                                hashMap.put("Admin", event.getAdmin());
+                                hashMap.put("EventName", event.getEventName());
+                                hashMap.put("EventTime", event.getEventName());
+                                hashMap.put("EventToken", event.getEventToken());
+                                hashMap.put("Location", event.getLocation());
+                                hashMap.put("Latitude", event.getLatitude());
+                                hashMap.put("Longitude", event.getLongitude());
+                                reference.child(token).child("EventInfo").setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        join.setText("Leave");
+                                    }
+                                });
+                            }
+
                         }
 
                         @Override
@@ -149,14 +266,19 @@ public class EventActivity extends AppCompatActivity {
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String name = snapshot.child("EventName").getValue().toString();
-                String t = snapshot.child("EventTime").getValue().toString();
-                String loc = snapshot.child("Location").getValue().toString();
-                String desc = snapshot.child("Description").getValue().toString();
-                eventname.setText(name);
-                time.setText("Time: " + t);
-                location.setText("Location: " + loc);
-                description.setText(desc);
+                // when we delete the event, we will run this function
+                // so we need to determine if value is null
+                if(snapshot.child("EventName").getValue() != null){
+                    String name = snapshot.child("EventName").getValue().toString();
+                    String t = snapshot.child("EventTime").getValue().toString();
+                    String loc = snapshot.child("Location").getValue().toString();
+                    String desc = snapshot.child("Description").getValue().toString();
+                    eventname.setText(name);
+                    time.setText("Time: " + t);
+                    location.setText("Location: " + loc);
+                    description.setText(desc);
+                }
+
             }
 
             @Override
@@ -198,5 +320,29 @@ public class EventActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void deleteAllUsersEvents(final String groupName, final String token) {
+
+        FirebaseDatabase.getInstance().getReference("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot user : snapshot.getChildren()) {
+                    if(user.child("Groups").child(groupName).getValue() != null){
+                        if(user.child("Groups").child(groupName).child(token).getValue() != null){
+                            user.child("Groups").child(groupName).child(token).getRef().removeValue();
+                        }
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
